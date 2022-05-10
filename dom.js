958df3a628a32d35cfa4aa6c76b1d75d41a9f55e
Giedre Valanciute <giedrevalanciute@Giedres-MacBook-Pro.local>
2022-05-10 19:36:26 +0100

    first commit

---
+++ dom.js
@@ -0,0 +1,11 @@
+let elementTag = document.getElementsByTagName("p");
+console.log(elementTag);
+
+let elementClass = document.getElementsByClassName("container");
+console.log(elementClass);
+
+let elementId = document.getElementById("title");
+console.log(elementId);
+
+let elementQuerySelector = document.querySelector(".container");
+console.log(elementQuerySelector); 
